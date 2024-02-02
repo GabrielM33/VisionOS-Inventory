12e0548e970d83f871b4729b75b91a9ef48116ac
gabrielm33 <gabrielmoraesweb@gmail.com>
2024-02-02 18:07:39 -0300

    iOS UI completed

---
+++ InventoryPrototypeVisionOS/Views/InventoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryListView.swift
+//  InventoryPrototypeVisionOS
+//
+//  Created by Gabriel Moraes on 02/02/24.
+//
+
+import SwiftUI
+
+struct InventoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InventoryListView()
+}
